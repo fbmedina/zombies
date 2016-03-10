ae802a854af428a3817adfd18bc2e05f2c6e5bce
Francisca Medina <fbmedina@uc.cl>
2016-03-09 21:53:58 -0300

    segundo commit

--- Creature.rb
+++ Creature.rb
@@ -3,8 +3,6 @@ require_relative "MAP.rb"
 class Creature
 
   @@names = ["Brent Pearson", "Sandy Vazquez", "Eli Bennett", "Ariana Martinez", "Rodolfo Stevens", "Mallory Molina", "Holly Park", "Roberto Salinas", "Miranda Vasquez", "Jackson Logan", "Antonio Moreno", "Devon Webb", "Emma Kim", "Jillian Wong", "Alice Montgomery", "Vanessa Shaw", "Casey Lawson", "Jaclyn Graves", "Brent Hoffman", "Jonathan Pearson", "Yvonne Campos", "Deanna Jensen", "Dennis Shaw", "Jessica Nichols", "Sabrina Ramsey", "Lance Welch", "Daniela Mack", "Madeleine Holt", "Dominick Elliott"]
-  @@positions_map_x = (1..MAP[0])
-  @@positions_map_y = (1..MAP[1])
 
   attr_reader :name
   attr_accessor :position_x, :position_y, :type
@@ -17,10 +15,12 @@ class Creature
   end
 
   def in_map
-    if @@positions_map_x.include?(@position_x) && @@positions_map_y.include?(@position_y)
-      return true
-    else
+    if position_x > MAP[0] || position_x < 1
       return false
+    elsif position_y > MAP[1] || position_y < 1
+      return false
+    else
+      return true
     end
   end
 
