a535a08de769b3cf7208317189d3b227d92df5f6
Francisca Medina <fbmedina@uc.cl>
2016-03-14 17:28:18 -0300

    se crearon métodos similares para la clase persona

--- Person.rb
+++ Person.rb
@@ -1,3 +1,31 @@
-class Person
-  
+require_relative "Creature.rb"
+
+class Person < Creature
+
+  def run
+    dentro_mapa_x = (1..MAP[0]).to_a
+    dentro_mapa_y = (1..MAP[1]).to_a
+    random_run = rand(-2..2)
+
+    if dentro_mapa_x.include?(self.position_x + random_run) 
+      self.position_x += random_run
+    end
+    if dentro_mapa_y.include?(self.position_y + random_run) 
+      self.position_y += random_run
+    end
+  end
+
+  def information
+    puts "La persona " + super
+  end
+
 end
+
+person1 = Person.new
+person1.information
+person1.run
+person1.information
+person1.run
+person1.information
+person1.run
+person1.information
